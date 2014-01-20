c7fa2d5442006d9dba642950ba1b17980c7526c2
campitos <jcampos@mexico-movil.com>
2014-01-20 10:22:42 -0600

    Lenguajes de Programacion Orientados a Objetos

---
+++ lenguajes/src/org/campitos/capitulo1/Circulo.java
@@ -0,0 +1,16 @@
+/*
+ * En esta primer clase aprenderas lo siguiente
+ 1. Abtrsaccion, encapsulamiento, clase miembros
+ 2. Creacion de objetos
+ 3. 
+ */
+
+package org.campitos.capitulo1;
+
+/**
+ *
+ * @author campitos
+ */
+public class Circulo {
+    
+}
