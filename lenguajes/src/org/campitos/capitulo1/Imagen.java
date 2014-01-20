bc06f17f2291e3bcfb1876add5eed748c3fcca64
campitos <jcampos@mexico-movil.com>
2014-01-20 11:25:03 -0600

    Lenguajes de Programacion Orientados a Objetos

--- lenguajes/src/org/campitos/capitulo1/Imagen.java
+++ lenguajes/src/org/campitos/capitulo1/Imagen.java
@@ -1,4 +1,4 @@
-/*Lunes 20 de enero
+/*Lunes 20 de enero de 2014
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
