1a15aa22a47cc1b7b184566322ad4d41c6b2abbb
campitos <jcampos@mexico-movil.com>
2014-01-19 23:57:08 -0600

    Lenguajes de Programacion Orientados a Objetos

--- lenguajes/src/lenguajes/Lenguajes.java
+++ lenguajes/src/lenguajes/Lenguajes.java
@@ -16,7 +16,7 @@ public class Lenguajes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("hola munndo");
+       System.out.println("Lenguajes de programación Orientados a Objetos");
     }
     
 }
