1823f7d5a656c718b0301229c2939414badd490a
campitos <jcampos@mexico-movil.com>
2014-01-27 08:57:40 -0600

    Actualizacion nomina

--- lenguajes/src/org/campitos/capitulo1/Circulo.java
+++ lenguajes/src/org/campitos/capitulo1/Circulo.java
@@ -12,5 +12,17 @@ package org.campitos.capitulo1;
  * @author campitos
  */
 public class Circulo {
+     private float radio;
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        if(radio<0)radio=-radio;
+        this.radio = radio;
+    }
+    
+    
     
 }
---
+++ lenguajes/src/org/campitos/capitulo1/TestCirculo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.campitos.capitulo1;
+
+/**
+ *
+ * @author campitos
+ */
+public class TestCirculo {
+    public static void main(String args[]){
+        //Creamos un Circulo
+        Circulo c=new Circulo();
+        c.setRadio(20);
+        //Imprimiendo el valor del radio probaremos que IMPIDIÓ meter valores negativos
+        System.out.println(c.getRadio());
+        
+    }
+    
+}
