4b61c8fc67633a554629bb2a560462307fed2673
campitos <jcampos@mexico-movil.com>
2014-01-27 22:05:05 -0600

    Agregamos todo lo de Spring para nomina

---
+++ lenguajes/src/org/campitos/capitulo1/TestUsuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.campitos.capitulo1;
+
+/**
+ *
+ * @author campitos
+ */
+public class TestUsuario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Usuario u=new Usuario();
+       u.setEdad(14);
+       System.out.println(u.getEdad());
+    }
+    
+}
--- lenguajes/src/org/campitos/capitulo1/Usuario.java
+++ lenguajes/src/org/campitos/capitulo1/Usuario.java
@@ -11,5 +11,24 @@ package org.campitos.capitulo1;
  * @author campitos
  */
 public class Usuario {
+    private String nombre;
+    private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if(edad<18)edad=18;
+        this.edad = edad;
+    }
     
 }
