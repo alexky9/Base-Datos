ca97ca5d9cbfd1b327eeaed3f8a6d57e680da276
campitos <jcampos@mexico-movil.com>
2014-03-16 17:51:30 -0600

    Capitulo 4

---
+++ lenguajes/src/org/campitos/capitulo2/ServicioCuenta.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.campitos.capitulo2;
+
+/**
+ *
+ * @author campitos
+ */
+public class ServicioCuenta {
+    InterfaceCuenta c;
+    public ServicioCuenta(Cuenta c){
+  
+                
+    
+       
+    }
+    
+    public void obtenerCuenta()throws Exception{
+        c.crearCuenta();
+    }
+    
+
+    
+}
